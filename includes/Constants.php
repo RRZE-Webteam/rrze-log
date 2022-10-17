718e258b2d426e8fb613538ab25ea453c2a97718
rvdforst <rolf.v.d.forst@fau.de>
2022-10-17 14:34:09 +0200

    Update Constants.php

--- includes/Constants.php
+++ includes/Constants.php
@@ -7,25 +7,19 @@ defined('ABSPATH') || exit;
 class Constants
 {
     /**
-     * [PLUGIN_PHP_VERSION description]
-     * @var string
-     */    
-    const PLUGIN_PHP_VERSION = '7.4';
-
-    /**
-     * [PLUGIN_WP_VERSION description]
-     * @var string
-     */    
-    const PLUGIN_WP_VERSION = '5.6';
-
-    /**
-     * [LOG_DIR description]
+     * Required PHP version.
      * @var string
      */
-    const LOG_DIR = WP_CONTENT_DIR . '/log/rrze-log';
+    const REQUIRED_PHP_VERSION = '7.4';
 
     /**
-     * [LOG_PATH description]
+     * Required WP version.
+     * @var string
+     */
+    const REQUIRED_WP_VERSION = '6.0';
+
+    /**
+     * Full log path.
      * @var string
      */
     const LOG_PATH = WP_CONTENT_DIR . '/log/rrze-log/';
