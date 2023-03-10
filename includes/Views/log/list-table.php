19d335c177437909b2389eaf4b66c5452a3ec03a
rvdforst <rolf.v.d.forst@fau.de>
2023-03-10 13:51:08 +0100

    Create list-table.php

---
+++ includes/Views/log/list-table.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace RRZE\Log;
+
+defined('ABSPATH') || exit;
+?>
+<h2>
+    <?php _e('Log', 'rrze-log'); ?>
+</h2>
+
+<form method="get">
+    <input type="hidden" name="page" value="<?php echo $_REQUEST['page'] ?>">
+    <input type="hidden" name="level" value="<?php echo $data['level'] ?>">
+    <input type="hidden" name="logfile" value="<?php echo $data['logfile'] ?>">
+    <?php $data['listTable']->search_box(__('Search'), 's'); ?>
+</form>
+
+<form method="get">
+    <input type="hidden" name="page" value="<?php echo $_REQUEST['page'] ?>">
+    <input type="hidden" name="s" value="<?php echo $data['s'] ?>">
+    <input type="hidden" name="logfile" value="<?php echo $data['logfile'] ?>">
+    <?php $data['listTable']->display(); ?>
+</form>
