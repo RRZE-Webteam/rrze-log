3e7e12ad79fc51a48d4b79064bd9dcc61ebd77aa
rvdforst <rolf.v.d.forst@fau.de>
2023-03-10 13:50:35 +0100

    Update ListTable.php

--- includes/ListTable.php
+++ includes/ListTable.php
@@ -14,12 +14,19 @@ use WP_List_Table;
 class ListTable extends WP_List_Table
 {
     /**
+     * Options values.
+     * @var object
+     */
+    public $options;
+
+    /**
      * [__construct description]
      */
     public function __construct()
     {
         global $status, $page;
 
+        $this->options = Options::getOptions();
         $this->items = [];
 
         parent::__construct([
@@ -46,17 +53,28 @@ class ListTable extends WP_List_Table
     }
 
     /**
+     * [column_siteurl description]
+     * @param  array $item [description]
+     * @return string       [description]
+     */
+    public function column_siteurl($item)
+    {
+        return untrailingslashit($item['siteurl']);
+    }
+
+    /**
      * [column_date description]
      * @param  array $item [description]
      * @return string       [description]
      */
-    public function column_datetime($item) {
+    public function column_datetime($item)
+    {
         return sprintf(
             '<span title="%1$s">%2$s</span>',
             get_date_from_gmt($item['datetime'], __('Y/m/d') . ' G:i:s.u'),
             get_date_from_gmt($item['datetime'], __('Y/m/d') . ' H:i:s')
         );
-	}
+    }
 
     /**
      * [get_columns description]
@@ -64,21 +82,26 @@ class ListTable extends WP_List_Table
      */
     public function get_columns()
     {
-        return [
+        $columns = [
             'level' => __('Error level', 'rrze-log'),
             'siteurl' => __('Website', 'rrze-log'),
             'message' => __('Message', 'rrze-log'),
             'datetime' => __('Date', 'rrze-log')
         ];
+        if (!is_network_admin() && $this->options->adminMenu) {
+            unset($columns['siteurl']);
+        }
+        return $columns;
     }
 
     /**
      * [single_row description]
      * @param  array $item [description]
      */
-    public function single_row($item) {
+    public function single_row($item)
+    {
         echo '<tr class="data">';
-        $this->single_row_columns( $item );
+        $this->single_row_columns($item);
         echo '</tr>';
         printf('<tr class="metadata metadata-hidden"> <td colspan=%d>', count($this->get_columns()));
         printf('<pre>%1$s</pre>', print_r($item, true));
@@ -92,7 +115,7 @@ class ListTable extends WP_List_Table
     public function prepare_items()
     {
         $s = !empty($_REQUEST['s']) ? array_map('trim', explode(' ', trim($_REQUEST['s']))) : '';
-        $level = !empty($_REQUEST['level']) && in_array($_REQUEST['level'], Logger::LEVELS) ? $_REQUEST['level'] : '';
+        $level = !empty($_REQUEST['level']) && in_array($_REQUEST['level'], CONSTANTS::LEVELS) ? $_REQUEST['level'] : '';
         $logFile = isset($_REQUEST['logfile']) && $this->verifyLogfileFormat($_REQUEST['logfile']) ? $_REQUEST['logfile'] : date('Y-m-d');
 
         $columns = $this->get_columns();
@@ -114,13 +137,16 @@ class ListTable extends WP_List_Table
             $search[] = '"level":"' . $level . '"';
         }
         $logParser = new LogParser($logFile, $search, (($currentPage - 1) * $perPage), $perPage);
-        $items = $logParser->getItems();
+        if (!is_network_admin() && $this->options->adminMenu) {
+            $items = $logParser->getItems('siteurl', site_url());
+        } else {
+            $items = $logParser->getItems();
+        }
         if (!is_wp_error($items)) {
             foreach ($items as $value) {
                 $this->items[] = json_decode($value, true);
             }
         }
-
         $totalItems = $logParser->getTotalLines();
 
         $this->set_pagination_args(
@@ -138,24 +164,24 @@ class ListTable extends WP_List_Table
      */
     protected function extra_tablenav($which)
     {
-        ?>
-		<div class="alignleft actions">
-		<?php
-        if ('top' === $which) {
-            ob_start();
+?>
+        <div class="alignleft actions">
+            <?php
+            if ('top' === $which) {
+                ob_start();
 
-            $this->levelsDropdown();
-            $this->logFilesDropdown();
+                $this->levelsDropdown();
+                $this->logFilesDropdown();
 
-            $output = ob_get_clean();
+                $output = ob_get_clean();
 
-            if (! empty($output)) {
-                echo $output;
-                submit_button(__('Filter'), '', 'filter_action', false, ['id' => 'rrze-log-level-submit']);
-            }
-        } ?>
-		</div>
-		<?php
+                if (!empty($output)) {
+                    echo $output;
+                    submit_button(__('Filter'), '', 'filter_action', false, ['id' => 'rrze-log-level-submit']);
+                }
+            } ?>
+        </div>
+    <?php
     }
 
     /**
@@ -166,12 +192,12 @@ class ListTable extends WP_List_Table
         $levelFilter = isset($_REQUEST['level']) ? $_REQUEST['level'] : ''; ?>
         <select id="levels-filter" name="level">
             <option value=""><?php _e('All error levels', 'rrze-log'); ?></option>
-            <?php foreach (Logger::LEVELS as $level) :
+            <?php foreach (CONSTANTS::LEVELS as $level) :
                 $selected = $levelFilter == $level ? ' selected = "selected"' : ''; ?>
-            <option value="<?php echo $level; ?>"<?php echo $selected; ?>><?php echo $level; ?></option>
+                <option value="<?php echo $level; ?>" <?php echo $selected; ?>><?php echo $level; ?></option>
             <?php endforeach; ?>
         </select>
-        <?php
+    <?php
     }
 
     /**
@@ -183,7 +209,7 @@ class ListTable extends WP_List_Table
         $logFiles = [];
         if (!is_dir(Constants::LOG_PATH)) {
             return;
-        }        
+        }
         foreach (new \DirectoryIterator(Constants::LOG_PATH) as $file) {
             if ($file->isFile()) {
                 $logfile = $file->getBasename('.' . $file->getExtension());
@@ -200,10 +226,10 @@ class ListTable extends WP_List_Table
         <select id="logfiles-filter" name="logfile">
             <?php foreach ($logFiles as $logfile) :
                 $selected = $logFilesFilter == $logfile ? ' selected = "selected"' : ''; ?>
-            <option value="<?php echo $logfile; ?>"<?php echo $selected; ?>><?php echo $logfile; ?></option>
+                <option value="<?php echo $logfile; ?>" <?php echo $selected; ?>><?php echo $logfile; ?></option>
             <?php endforeach; ?>
         </select>
-        <?php
+<?php
     }
 
     /**
