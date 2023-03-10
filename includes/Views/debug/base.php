e776a3a05598fb004ae4691c614147206502d8e9
rvdforst <rolf.v.d.forst@fau.de>
2023-03-10 13:50:58 +0100

    Create base.php

---
+++ includes/Views/debug/base.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace RRZE\Log;
+
+defined('ABSPATH') || exit;
+?>
+<div class="wrap">
+    <?php
+    foreach ($data['messages'] as $message) :
+        if (is_wp_error($message)) : ?>
+            <div class="error">
+                <p>
+                    <?php printf(
+                        /* translators: %s: Error message. */
+                        __('Error: %s', 'rrze-log'),
+                        $message->get_error_message()
+                    );
+                    ?>
+                </p>
+            </div>
+        <?php else : ?>
+            <div class="updated">
+                <p><?php echo $message; ?></p>
+            </div>
+    <?php endif;
+    endforeach;
+
+    include $view . '.php';
+    ?>
+</div>
