26910fbfc5fb71143c2cbe4c466422a7d7330ff1
rvdforst <rolf.v.d.forst@fau.de>
2024-02-23 16:39:50 +0100

    Update Utils.php

--- includes/Utils.php
+++ includes/Utils.php
@@ -40,47 +40,12 @@ class Utils
     }
 
     /**
-     * Log errors by writing to the debug.log file.
+     * Verify if the string has the correct 'yyyy-mm-dd' format.
+     * @param  string $string 'yyyy-mm-dd' format
+     * @return boolean
      */
-    public static function debug($input, string $level = 'i')
+    public static function verifyLogfileFormat($string)
     {
-        if (!defined('WP_DEBUG') || !WP_DEBUG) {
-            return;
-        }
-        if (in_array(strtolower((string) WP_DEBUG_LOG), ['true', '1'], true)) {
-            $logPath = WP_CONTENT_DIR . '/debug.log';
-        } elseif (is_string(WP_DEBUG_LOG)) {
-            $logPath = WP_DEBUG_LOG;
-        } else {
-            return;
-        }
-        if (is_array($input) || is_object($input)) {
-            $input = print_r($input, true);
-        }
-        switch (strtolower($level)) {
-            case 'e':
-            case 'error':
-                $level = 'Error';
-                break;
-            case 'i':
-            case 'info':
-                $level = 'Info';
-                break;
-            case 'd':
-            case 'debug':
-                $level = 'Debug';
-                break;
-            default:
-                $level = 'Info';
-        }
-        error_log(
-            date("[d-M-Y H:i:s \U\T\C]")
-                . " WP $level: "
-                . basename(__FILE__) . ' '
-                . $input
-                . PHP_EOL,
-            3,
-            $logPath
-        );
-    }
+        return preg_match("/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/", $string);
+    }    
 }
