3284b5d2fad4326a954ea1425f60a9d200878da9
rvdforst <rolf.v.d.forst@fau.de>
2024-04-10 17:04:34 +0200

    Update Utils.php

--- includes/Utils.php
+++ includes/Utils.php
@@ -21,6 +21,10 @@ class Utils
         return json_last_error() === JSON_ERROR_NONE;
     }
 
+    /**
+     * Check if the WP_DEBUG_LOG constant is defined and has the correct value.
+     * @return boolean|\WP_Error
+     */
     public static function isDebugLog()
     {
         if (!defined('WP_DEBUG') || !WP_DEBUG) {
@@ -47,5 +51,63 @@ class Utils
     public static function verifyLogfileFormat($string)
     {
         return preg_match("/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/", $string);
-    }    
+    }
+
+    /**
+     * Get log items.
+     * @param  array $args
+     * @return array
+     */
+    public static function getLogs($args = [])
+    {
+        $search = $args['search'] ?? [];
+        $limit = $args['limit'] ?? -1;
+        $offset = $args['offset'] ?? 0;      
+
+        return self::getLog('', $search, $offset, $limit)['items'] ?? [];
+    }
+
+    /**
+     * Get the log items.
+     * @param  string $logFile
+     * @param  array $search
+     * @param  integer $offset
+     * @param  integer $count
+     * @return array
+     */
+    public static function getLog($logFile = '', $search = [], $offset = 0, $count = -1)
+    {
+        $logPath = Constants::LOG_PATH;
+        if (!self::verifyLogfileFormat($logFile)) {
+            $logFile = date('Y-m-d');
+        }
+        $logFile = sprintf('%1$s%2$s.log', $logPath, $logFile);
+        $search = is_array($search) ?
+            array_filter(array_map('trim', $search)) :
+            [];
+        $offset = absint($offset);
+        $count = $count < 0 ? -1 : absint($count);
+
+        $logParser = new LogParser($logFile, $search, $offset, $count);
+
+        if (!is_network_admin()) {
+            $logItems = $logParser->getItems('siteurl', site_url());
+        } else {
+            $logItems = $logParser->getItems();
+        }
+
+        $items = [];
+        if (!is_wp_error($logItems)) {
+            foreach ($logItems as $item) {
+                $items[] = json_decode($item, true);
+            }
+        }
+
+        $totalItems = $logParser->getTotalLines();
+
+        return [
+            'items' => $items,
+            'total_items' => $totalItems
+        ];
+    }
 }
