9a628b84228b78734504e86b4b6bc755855e9376
Rolf <rolf.v.d.forst@fau.de>
2025-08-28 16:12:34 +0200

    Update ListTable.php

--- includes/ListTable.php
+++ includes/ListTable.php
@@ -18,23 +18,13 @@ class ListTable extends WP_List_Table
     public $options;
 
     /**
-     * Absolut Log Path
-     *
-     * @var string
-     */
-    protected $logPath;
-
-    /**
      * Constructor
-     *
-     * @param string $logPath
      */
-    public function __construct(string $logPath)
+    public function __construct()
     {
         global $status, $page;
 
         $this->options = Options::getOptions();
-        $this->logPath = $logPath;
         $this->items = [];
 
         parent::__construct([
@@ -136,7 +126,6 @@ class ListTable extends WP_List_Table
                 ob_start();
 
                 $this->levelsDropdown();
-                $this->logFilesDropdown();
 
                 $output = ob_get_clean();
 
@@ -163,37 +152,5 @@ class ListTable extends WP_List_Table
             <?php endforeach; ?>
         </select>
     <?php
-    }
-
-    /**
-     * Dropdown with log files.
-     */
-    protected function logFilesDropdown()
-    {
-        $logFilesFilter = isset($_REQUEST['logfile']) ? $_REQUEST['logfile'] : date('Y-m-d');
-        $logFiles = [];
-        if (!is_dir($this->logPath)) {
-            return;
-        }
-        foreach (new \DirectoryIterator($this->logPath) as $file) {
-            if ($file->isFile()) {
-                $logfile = $file->getBasename('.' . $file->getExtension());
-                if (Utils::verifyLogfileFormat($logfile)) {
-                    $logFiles[$logfile] = $logfile;
-                }
-            }
-        }
-
-        if (count($logFiles) < 2) {
-            return;
-        }
-        krsort($logFiles); ?>
-        <select id="logfiles-filter" name="logfile">
-            <?php foreach ($logFiles as $logfile) :
-                $selected = $logFilesFilter == $logfile ? ' selected = "selected"' : ''; ?>
-                <option value="<?php echo $logfile; ?>" <?php echo $selected; ?>><?php echo $logfile; ?></option>
-            <?php endforeach; ?>
-        </select>
-<?php
     }
 }
