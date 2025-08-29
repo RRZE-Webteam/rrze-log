c37ee4c8a8eda33d6f8296e8e417ca22c5cd2ee0
Rolf <rolf.v.d.forst@fau.de>
2025-08-29 10:44:54 +0200

    Update Options.php

--- includes/Options.php
+++ includes/Options.php
@@ -20,9 +20,9 @@ class Options
     {
         $options = [
             'enabled' => '0',
-            'maxLines' => 5000,
+            'maxLines' => 10000,
             'adminMenu' => '0',
-            'debugMaxLines' => 5000,
+            'debugMaxLines' => 10000,
             'debugLogAccess' => ''
         ];
 
