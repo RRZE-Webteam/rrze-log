3a9d43cbb81600733959f77ae6032b36b760a980
Wolfgang Wiese <xwolf@xwolf.de>
2026-02-07 21:24:43 +0100

    Aufräumen in der optik

--- includes/Views/list-table.php
+++ includes/Views/list-table.php
@@ -1,23 +1,63 @@
 <?php
 
-namespace RRZE\Log;
-
 defined('ABSPATH') || exit;
+
+$title = isset($data['title']) ? (string) $data['title'] : '';
+$listTable = isset($data['listTable']) ? $data['listTable'] : null;
+
+$action = isset($data['action']) ? (string) $data['action'] : 'index';
+$s = isset($data['s']) ? (string) $data['s'] : '';
+$level = isset($data['level']) ? (string) $data['level'] : '';
+$logfile = isset($data['logfile']) ? (string) $data['logfile'] : '';
+
+if ($logfile === '') {
+    $logfile = date('Y-m-d');
+}
+
 ?>
-<h2>
-    <?php echo $data['title']; ?>
-</h2>
+<div class="wrap">
+    <h1><?php echo esc_html($title); ?></h1>
 
-<form method="get">
-    <input type="hidden" name="page" value="<?php echo $_REQUEST['page'] ?>">
-    <input type="hidden" name="level" value="<?php echo $data['level'] ?>">
-    <input type="hidden" name="logfile" value="<?php echo $data['logfile'] ?>">
-    <?php $data['listTable']->search_box(__('Search'), 's'); ?>
-</form>
+    <?php if (!empty($data['messages']) && is_array($data['messages'])): ?>
+        <?php foreach ($data['messages'] as $msg): ?>
+            <div class="notice notice-info is-dismissible">
+                <p><?php echo esc_html((string) $msg); ?></p>
+            </div>
+        <?php endforeach; ?>
+    <?php endif; ?>
 
-<form method="get">
-    <input type="hidden" name="page" value="<?php echo $_REQUEST['page'] ?>">
-    <input type="hidden" name="s" value="<?php echo $data['s'] ?>">
-    <input type="hidden" name="logfile" value="<?php echo $data['logfile'] ?>">
-    <?php $data['listTable']->display(); ?>
-</form>
+    <?php if ($listTable instanceof WP_List_Table): ?>
+        <form method="get">
+            <?php
+            foreach ($_GET as $key => $value) {
+                if ($key === 's') {
+                    continue;
+                }
+                if (is_array($value)) {
+                    continue;
+                }
+                printf(
+                    '<input type="hidden" name="%s" value="%s">',
+                    esc_attr((string) $key),
+                    esc_attr((string) $value)
+                );
+            }
+            ?>
+            <input type="hidden" name="page" value="<?php echo esc_attr(isset($_REQUEST['page']) ? (string) $_REQUEST['page'] : 'rrze-log'); ?>">
+
+            <?php
+            $listTable->search_box(__('Search', 'rrze-log'), 'rrze-log-search');
+
+            if (method_exists($listTable, 'views')) {
+                $listTable->views();
+            }
+
+            $listTable->display();
+            ?>
+        </form>
+    <?php else: ?>
+        <div class="notice notice-error">
+            <p><?php echo esc_html__('List table is not available.', 'rrze-log'); ?></p>
+        </div>
+    <?php endif; ?>
+</div>
